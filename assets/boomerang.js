b033f2a09a28c232a0cc3e4c342a24cb0d342879
Devren Freestone <Dev@MacBook-Pro.local>
2017-05-24 20:22:41 -0700

    ready to add API

--- assets/boomerang.js
+++ assets/boomerang.js
@@ -1,1 +1,12 @@
 console.log("linked");
+
+//Global Vars
+var search = $("#search")
+var resultsOne = $("#resultsOne");
+var resultsTwo = $("#resultsTwo");
+var resultsArray
+
+//Creates the wells in the resultsOne for the search
+$(search).on("click", function({
+
+}))
